8fa31e9bb1864fc248eb105ec8900aa7179b9a6d
faridaayu <faridaayud@gmail.com>
2020-04-17 15:29:55 +0700

    detail kegiatan harian

--- application/controllers/KASI/Kegiatan_staf.php
+++ application/controllers/KASI/Kegiatan_staf.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kegiatan_staf extends CI_Controller {
 
-	
+
 	public function index()
 	{
 		$this->load->view('KASI/v_kegiatan_staf');
--- application/views/KABID/v_sidebar_kabid.php
+++ application/views/KABID/v_sidebar_kabid.php
@@ -14,7 +14,7 @@
                                 <li><a href="<?php echo base_url().'KABID/Data_staf'?>">Data Staf </a></li>
                             </ul>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-doc"></i><span class="hide-menu">Laporan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kegiatan Harian</span></a>
+                        <li> <a class="waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-doc"></i><span class="hide-menu">Laporan Kegiatan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Harian Staf</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="<?php echo base_url().'KABID/Profil_kabid'?>" aria-expanded="false"><i class="ti-user"></i><span class="hide-menu">Profil Saya</span></a>
                         </li>
