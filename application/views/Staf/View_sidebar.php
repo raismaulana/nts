e5e7646231d1104e8cf3a4d25a8b4e5e6b35f312
KursitaDewiSafitri <kursitadewi36@gmail.com>
2020-05-12 15:22:52 +0700

    hak akses staf dan kabid

--- application/views/Staf/View_sidebar.php
+++ application/views/Staf/View_sidebar.php
@@ -6,7 +6,7 @@
                     <ul id="sidebarnav">
                         <li> <a class="waves-effect waves-dark" href="<?php echo base_url().'staf/Beranda'?>" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Beranda </span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="<?php echo base_url().'staf/Pencatatan'?>" aria-expanded="false"><i class="ti-pencil-alt"></i><span class="hide-menu">Pencatatan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kegiatan Harian</span></a>
+                        <li> <a class="waves-effect waves-dark" href="<?php echo base_url().'staf/Kegiatan_staf'?>" aria-expanded="false"><i class="ti-pencil-alt"></i><span class="hide-menu">Pencatatan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kegiatan Harian</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="<?php echo base_url().'staf/Data_Staf'?>" aria-expanded="false"><i class="icon-people"></i><span class="hide-menu">Data Pegawai</span></a>
                         </li>
